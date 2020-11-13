5e0c82f26e6f661f729d5e691415cb7c4cc425d4
Conner Marzen <conner.marzen@collins.com>
2020-11-13 13:52:47 -0600

    Revamped Python output to be more orthogonal.

--- protogen/compiler.py
+++ protogen/compiler.py
@@ -8,9 +8,12 @@ from protogen.library.std import STANDARD_TYPES
 from protogen.util import PGFile, PyClass
 
 
-class PythonCompiler(object):
+# Note this is being obseleted.
+class _PythonCompiler(object):
 
     def __init__(self, inFiles: List[str], outDir: str, verbose: bool = False):
+        raise DeprecationWarning
+
         self._parser = PGParser(inFiles)
         self._parser.parse()
 
@@ -287,3 +290,147 @@ class NodeJSCompiler(object):
         self.printFactory(out, file)
         out.write("\n")
         self.printExports(out, file)
+
+
+class PythonCompiler(object):
+
+    def __init__(self, inFiles: List[str], outDir: str, verbose: bool = False):
+        self._parser = PGParser(inFiles)
+        self._parser.parse()
+
+        if not os.path.exists(outDir):
+            os.makedirs(outDir)
+        # remove tailing slash for consistency (I will add them back in)
+        self.outDir = outDir.rstrip('/')
+
+        self.classes: List[PyClass] = []
+        self.files: List[PGFile] = self._parser.transform()
+        del(self._parser)  # save memory, the parser is dense and repetitive
+
+        # TODO: Work through parser and consolidate into PythonCompiler
+
+    def compile(self):
+        for item in self.files:
+            print('Compiling {} into {}/{}_proto.py'
+                  ''.format(item.filename, self.outDir, item.header))
+            file = open(self.outDir + '/' + item.header + '_proto.py', 'w')
+            self.generateCode(out=file, file=item)
+            file.write(os.linesep)
+            file.close()
+
+    def printClass(self, out: TextIOWrapper, file: PGFile, pyClass: PyClass,
+                   indent: int, root: bool):
+        tab = '    '
+        if root:
+            out.write("\nclass {}(Serializable, "
+                        "Printable):\n".format(pyClass.name))
+        else:
+            out.write("\n{}class {}(Serializable, Printable):\n".format(
+                tab*indent, pyClass.name))
+        out.write("\n{}def __init__(self, data: dict = None):\n"
+                  .format(tab*(indent+1)))
+        self.printAttributes(out, file, pyClass, indent+1)
+        out.write("\n{}if data is not None:\n".format(tab*(indent+2)))
+        for item in file.declarations:
+            if util.inferParentClass(item) == pyClass.fqname:
+                short = util.inferShortName(item)
+                type, required = file.declarations[item]
+                if type in STANDARD_TYPES.values():
+                    out.write("{}self.data['{}'][0] = data['{}']\n".format(tab*(indent+3), short, short))
+                # local, nested class (needs 'self')
+                elif type in pyClass.gatherSubclasses('name'):
+                    out.write("{}self.data['{}'][0] = self.{}(data['{}'])\n".format(tab*(indent+3), short, type, short))
+                # local, non-nested class (doesn't need 'self')
+                else:
+                    out.write("{}self.data['{}'][0] = {}(data['{}'])\n".format(tab*(indent+3), short, type, short))
+
+
+        for item in pyClass.subclasses:
+            self.printClass(out, file, item, indent+1, False)
+
+        self.printMethods(out, file, pyClass, indent+1)
+        out.write("{}# End Class {}\n".format(tab*indent, pyClass.name))
+
+    def printAttributes(self, out: TextIOWrapper, file: PGFile, pyClass: PyClass, indent: int):
+        tab = '    '
+        out.write('{}self.data = {{\n'.format(tab*(indent+1)))
+        for item in file.declarations:
+            if util.inferParentClass(item) == pyClass.fqname:
+                # type, required = file.declarations[item][0], file.declarations[item][1]
+                type, required = file.declarations[item]
+                short = util.inferShortName(item)
+                # primitive data type
+                if type in STANDARD_TYPES.values():
+                    out.write('{}\'{}\': [None, {}, False],\n'.format(
+                        tab*(indent+2), short, required))
+
+                # local, nested class (needs 'self')
+                elif type in pyClass.gatherSubclasses('name'):
+                    out.write('{}\'{}\': [self.{}(), {}, True],\n'.format(
+                        tab*(indent+2), short, type, required))
+
+                # local, non-nested class (doesn't need 'self')
+                else:
+                    out.write('{}\'{}\': [{}(), {}, True],\n'.format(
+                        tab*(indent+2), short, type, required))
+
+        out.write('{}}}\n'.format(tab*(indent+1)))
+
+    def printMethods(self, out: TextIOWrapper, file: PGFile, pyClass: PyClass, indent: int):
+        tab = '    '
+        for item in file.declarations:
+            if util.inferParentClass(item) == pyClass.fqname:
+                thing = file.declarations[item]
+                short = util.inferShortName(item)
+
+                # Get methods
+                if thing[0] in STANDARD_TYPES.values():
+                    out.write('\n{}def get_{}(self) -> {}:\n'.format(
+                        (tab*indent), short, thing[0]))
+                else:
+                    out.write('\n{}def get_{}(self) -> {}:\n'.format(
+                        (tab*indent), short, thing[0]))
+                out.write('{}return self.data[\'{}\'][0]\n'.format(
+                    (tab*(indent+1)), short))
+
+                # Set methods
+                out.write('\n{}def set_{}(self, {}: {}) -> \'{}\':\n'.format(
+                    (tab*indent), short,
+                    short, thing[0], pyClass.name))
+                out.write('{}self.data[\'{}\'][0] = {}\n'.format(
+                    (tab*(indent+1)), short, short))
+                out.write('{}return self\n'.format((tab*(indent+1))))
+
+    def printFactory(self, out: TextIOWrapper, file: PGFile):
+        tab = '    '
+        outString = (
+            "\n\nclass {}Factory(object):\n"
+            "    @staticmethod\n"
+            "    def deserialize(data: bytes):\n"
+            "        data = Serializable.deserialize(data)\n"
+            "        if len(data) > 1:\n"
+            "            raise AttributeError('This is likely not a Protogen packet.')\n"
+            "\n"
+            "        packetType = None\n"
+            "        for item in data:\n"
+            "            packetType = item[item.rfind('.')+1:]\n"
+        )
+        out.write(outString.format(file.header))
+        for item in file.classes:
+            if item.parent is None:  # root-level class
+                out.write('{}if packetType == \'{}\':\n'.format(
+                    tab*3, item.name))
+                out.write('{}return {}(data[item])\n'.format(tab*4, item.name))
+        outString = (
+            "            else:\n"
+            "                raise AttributeError('Respective class not found.')\n"
+        )
+        out.write(outString)
+
+    def generateCode(self, out: TextIOWrapper, file: PGFile, indent: str = '    '):
+        out.write("from protogen.library.message import Serializable\n"),
+        out.write("from protogen.library.message import Printable\n\n")
+        for item in file.classes:
+            if item.parent is None:
+                self.printClass(out, file, item, 0, True)
+        self.printFactory(out, file)
--- protogen/library/message.py
+++ protogen/library/message.py
@@ -3,136 +3,47 @@ from pprint import pprint
 import msgpack
 
 
-class Serializable(object):
-    def serialize(self):
-        return msgpack.packb(self._encode())
+class ValidationError(Exception):
+    def __init__(self, var_name):
+        super().__init__("{} is marked as required, yet is missing its value.".format(var_name))
 
+class Serializable(object):
     @staticmethod
     def deserialize(data: bytes) -> Dict:
         return msgpack.unpackb(data, raw=False)
 
-    def __encode(self, data=None, out: Dict = None) -> Dict:
-        out = dict()
+    def serialize(self):
+        return msgpack.packb({self.__class__.__name__: self._build_data_dict()})
 
+    def _build_data_dict(self, output = None, data = None):
         if data is None:
-            data = vars(self)
-        else:
-            data = vars(data)
-
+            data = self.data
+            output = {}
         for item in data:
-            if not isinstance(data[item], (int, float, str, list, dict, set, type(None))):
-                if item.startswith('_r_'):
-                    out[item[3:]] = (self.__encode(
-                        data=data[item], out=out), True)
-                elif item.startswith('_o_'):
-                    out[item[3:]] = (self.__encode(
-                        data=data[item], out=out), False)
+            if data[item][2]:
+                output[item] = {}
+                self._build_data_dict(output[item], data[item][0].data)
             else:
-                if item.startswith('_r_'):
-                    out[item[3:]] = (data[item], True)
-                elif item.startswith('_o_'):
-                    out[item[3:]] = (data[item], False)
-        return out
-
-    def _encode(self) -> Dict:
-        finalOut = self.__encode()
-        return {
-            self.__class__.__module__+'.'+self.__class__.__name__: finalOut
-        }
-
-    @staticmethod
-    def _init(node, data):
-        for item in data:
-            req = ('_r_' if data[item][1] else '_o_') + item
-            if type(data[item][0]) is dict:
-                Serializable._init(getattr(node, req), data[item][0])
-            else:
-                setattr(node, req, data[item][0])
-
+                # if data[item][1] and not data[item][0]:
+                #     raise ValidationError(item)
+                output[item] = data[item][0]
+        return output
 
 class Printable(object):
-
-    def _indent(self, level: int):
-        if level > 0:
-            return '    '*(level-1) + ' \u2514' + '\u2500'*2
-        return ''
-
-    def _str(self, data=None, indent: int = 0) -> str:
-        if data is None:
-            data = vars(self)
-        else:
-            data = vars(data)
-        outString = ''
-
-        for item in data:
-            if not isinstance(data[item], (int, float, str, list, dict, set, type(None))):
-                # the object is a recursive type.
-                if indent > 0:
-                    if item.startswith('_o_'):
-                        outString += '{}{:<20} \n{}'.format(
-                            self._indent(indent),
-                            item[3:] + ' (opt)',
-                            self._str(data[item], indent+1))
-                    elif item.startswith('_r_'):
-                        outString += '{}{:<20} \n{}'.format(
-                            self._indent(indent),
-                            item[3:] + ' (req)',
-                            self._str(data[item], indent+1))
-                else:
-                    if item.startswith('_o_'):
-                        outString += '{}{:<20} \n{}'.format(
-                            '    '*indent,
-                            item[3:] + ' (opt)',
-                            self._str(data[item], indent+1))
-                    elif item.startswith('_r_'):
-                        outString += '{}{:<20} \n{}'.format(
-                            '    '*indent,
-                            item[3:] + ' (req)',
-                            self._str(data[item], indent+1))
-            else:
-                if indent > 0:
-                    if item.startswith('_o_'):
-                        outString += '{}\u2192 {:<20}: {} (opt)\n'.format(
-                            self._indent(indent),
-                            item[3:],
-                            data[item])
-                    elif item.startswith('_r_'):
-                        outString += '{}\u2192 {:<20}: {} (req) \n'.format(
-                            self._indent(indent),
-                            item[3:],
-                            data[item])
-                else:
-                    if item.startswith('_o_'):
-                        outString += '{}{:<20}: {} (opt)\n'.format(
-                            self._indent(indent),
-                            item[3:],
-                            data[item])
-                    elif item.startswith('_r_'):
-                        outString += '{}{:<20}: {} (req) \n'.format(
-                            self._indent(indent),
-                            item[3:],
-                            data[item])
-        return outString
-
-    def __str__(self):
-        # return self._str()
+    def __str__(self) -> str:
         return self._str()
 
+    def _req(self, data):
+        return 'req' if data[1] else 'opt'
 
-class Validatable(object):
-    def __init__(self):
-        pass
-
-
-class Message(object):
-    def _filterVars(self, data) -> bool:
-        if data.startswith('_o_') or data.startswith('_r_'):
-            return True
-        return False
-
-    def matchAttribute(self, varName: str):
-        for item in filter(self._filterVars, vars(self)):
-            if item[3:][3:] == varName:
-                return item
-        raise AttributeError('Variable name \'{}\' not found in {}.'
-                             .format(varName, self.__class__.__name__))
+    def _str(self, data=None, indent=0):
+        tab = '    '
+        output = ''
+        if data is None:
+            data = self.data
+        for item in data:
+            if data[item][2]:
+                output += "{}{} ({}): \n{}".format(tab*indent, item, self._req(data[item]), self._str(data[item][0].data, indent+1))
+            else:
+                output += "{}{} ({}): {}\n".format(tab*indent, item, self._req(data[item]), data[item][0])
+        return output
